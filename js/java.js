3214562f2e39453b681122b30dfc568720bfec80
juancruzzg <75743291+juancruzzg@users.noreply.github.com>
2020-12-11 14:06:03 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,8 @@
+let menuToggle = document.querySelector('.menu-toggle')
+let menuToggleIcon = document.querySelector('.menu-toggle i')
+let menuToggle = document.getElementById('menu')
+
+
+menuToggle.addEventListener('click', e=>{
+	menu.classList.toggle('show');
+})
